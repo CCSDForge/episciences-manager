a00b68bb26636b5f08d75983084f737794c0730b
ZHAO <zhaoclaire11@gmail.com>
2025-05-20 10:52:20 +0200

    Créer un UserProvider pour récupérer les utilisateurs à partir d’un service externe

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-final class DefaultController extends AbstractController
+class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    #[Route('/login', name:'login', methods: ['GET'])]
+    public function login(Request $request) {
+        $target = urlencode($this->getParameter('cas_login_target').'/force');
+        $url = 'https://'.$this->getParameter('cas_host') . ((($this->getParameter('cas_port')!=80) || ($this->getParameter('cas_port')!=443)) ? ":".$this->getParameter('cas_port') : "") . $this->getParameter('cas_path') . '/login?service=';
+
+        return $this->redirect($url . $target);
+    }
+
+    #[Route('/logout', name:'logout', methods: ['GET'])]
+    public function logout(Request $request) {
+        if (($this->getParameter('cas_logout_target') !== null) && (!empty($this->getParameter('cas_logout_target')))) {
+            \phpCAS::logoutWithRedirectService($this->getParameter('cas_logout_target'));
+        } else {
+            \phpCAS::logout();
+        }
+    }
+
+    #[Route('/force', name:'force', methods: ['GET'])]
+    public function force(Request $request) {
+
+        if ($this->getParameter("cas_gateway")) {
+            if (!isset($_SESSION)) {
+                session_start();
+            }
+
+            session_destroy();
+        }
+
+        return $this->redirect($this->generateUrl('index'));
+    }
+
+
+    #[Route('/', name:'index', methods: ['GET'])]
+    public function index(Request $request) : Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        dump($this->container->get('security.token_storage'));
+        dump($this->getUser());
+
+        return $this->render('base.html.twig', []);
     }
 }
