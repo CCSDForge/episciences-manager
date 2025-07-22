dbfba521184751207b51abf50ad25f97d75fbe4a
ZHAO <zhaoclaire11@gmail.com>
2025-07-22 15:30:29 +0200

    Corriger la langue par défaut afin de garantir la cohérence

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,5 @@
 import './bootstrap.js';
 
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 /*
  * Welcome to your app's main JavaScript file!
  *
