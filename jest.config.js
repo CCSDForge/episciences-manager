4556a0fcfa69a97787762de5f24ba2921195fb4a
ZHAO <zhaoclaire11@gmail.com>
2025-08-27 11:39:14 +0200

    fix: resolve code formatting and linting issues across project

--- jest.config.js
+++ jest.config.js
@@ -3,27 +3,27 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/tests/javascript/jest/setup.js'],
   testMatch: [
     '<rootDir>/tests/javascript/**/*.test.js',
-    '!<rootDir>/tests/javascript/e2e/**/*.spec.js'
+    '!<rootDir>/tests/javascript/e2e/**/*.spec.js',
   ],
   collectCoverageFrom: [
     'assets/**/*.js',
     '!assets/vendor/**',
     '!assets/node_modules/**',
     '!assets/app.js',
-    '!assets/bootstrap.js'
+    '!assets/bootstrap.js',
   ],
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   coverageThreshold: {
     global: {
       branches: 0,
       functions: 0,
       lines: 0,
-      statements: 0
-    }
-  }
+      statements: 0,
+    },
+  },
 };
