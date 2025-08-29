09c33ade214f61f4922fe42d663bf889a8352673
ZHAO <zhaoclaire11@gmail.com>
2025-08-29 14:38:36 +0200

    disable color for CkEditor

--- assets/scripts/components/ckeditor.js
+++ assets/scripts/components/ckeditor.js
@@ -17,8 +17,8 @@ import {
   RemoveFormat,    // basicstyles > cleanup
   FontSize,        // styles
   FontFamily,      // styles
-  FontColor,       // colors
-  FontBackgroundColor, // colors
+  // FontColor,       // colors - DISABLED
+  // FontBackgroundColor, // colors - DISABLED
   SpecialCharacters,   // insert
   HorizontalLine,      // insert
   Image,               // insert
@@ -75,9 +75,9 @@ export function initializeCKEditor(elementId, placeholder = '') {
         FontSize,
         FontFamily,
 
-        // Colors (colors group)
-        FontColor,
-        FontBackgroundColor,
+        // Colors (colors group) - DISABLED
+        // FontColor,
+        // FontBackgroundColor,
 
         // Editing (editing group)
         FindAndReplace,
@@ -98,10 +98,10 @@ export function initializeCKEditor(elementId, placeholder = '') {
           'removeFormat',
           '|',
 
-          // Colors
-          'fontColor',
-          'fontBackgroundColor',
-          '|',
+          // Colors - DISABLED
+          // 'fontColor',
+          // 'fontBackgroundColor',
+          // '|',
 
           // Paragraph
           'alignment',
@@ -138,142 +138,18 @@ export function initializeCKEditor(elementId, placeholder = '') {
         ],
       },
 
-      // Configuration des couleurs
-      fontColor: {
-        colors: [
-          {
-            color: 'hsl(0, 0%, 0%)',
-            label: 'Black',
-          },
-          {
-            color: 'hsl(0, 0%, 30%)',
-            label: 'Dim grey',
-          },
-          {
-            color: 'hsl(0, 0%, 60%)',
-            label: 'Grey',
-          },
-          {
-            color: 'hsl(0, 0%, 90%)',
-            label: 'Light grey',
-          },
-          {
-            color: 'hsl(0, 0%, 100%)',
-            label: 'White',
-            hasBorder: true,
-          },
-          {
-            color: 'hsl(0, 75%, 60%)',
-            label: 'Red',
-          },
-          {
-            color: 'hsl(30, 75%, 60%)',
-            label: 'Orange',
-          },
-          {
-            color: 'hsl(60, 75%, 60%)',
-            label: 'Yellow',
-          },
-          {
-            color: 'hsl(90, 75%, 60%)',
-            label: 'Light green',
-          },
-          {
-            color: 'hsl(120, 75%, 60%)',
-            label: 'Green',
-          },
-          {
-            color: 'hsl(150, 75%, 60%)',
-            label: 'Aquamarine',
-          },
-          {
-            color: 'hsl(180, 75%, 60%)',
-            label: 'Turquoise',
-          },
-          {
-            color: 'hsl(210, 75%, 60%)',
-            label: 'Light blue',
-          },
-          {
-            color: 'hsl(240, 75%, 60%)',
-            label: 'Blue',
-          },
-          {
-            color: 'hsl(270, 75%, 60%)',
-            label: 'Purple',
-          },
-        ],
-        columns: 5,
-        documentColors: 10,
-      },
-
-      fontBackgroundColor: {
-        colors: [
-          {
-            color: 'hsl(0, 0%, 0%)',
-            label: 'Black',
-          },
-          {
-            color: 'hsl(0, 0%, 30%)',
-            label: 'Dim grey',
-          },
-          {
-            color: 'hsl(0, 0%, 60%)',
-            label: 'Grey',
-          },
-          {
-            color: 'hsl(0, 0%, 90%)',
-            label: 'Light grey',
-          },
-          {
-            color: 'hsl(0, 0%, 100%)',
-            label: 'White',
-            hasBorder: true,
-          },
-          {
-            color: 'hsl(0, 75%, 60%)',
-            label: 'Red',
-          },
-          {
-            color: 'hsl(30, 75%, 60%)',
-            label: 'Orange',
-          },
-          {
-            color: 'hsl(60, 75%, 60%)',
-            label: 'Yellow',
-          },
-          {
-            color: 'hsl(90, 75%, 60%)',
-            label: 'Light green',
-          },
-          {
-            color: 'hsl(120, 75%, 60%)',
-            label: 'Green',
-          },
-          {
-            color: 'hsl(150, 75%, 60%)',
-            label: 'Aquamarine',
-          },
-          {
-            color: 'hsl(180, 75%, 60%)',
-            label: 'Turquoise',
-          },
-          {
-            color: 'hsl(210, 75%, 60%)',
-            label: 'Light blue',
-          },
-          {
-            color: 'hsl(240, 75%, 60%)',
-            label: 'Blue',
-          },
-          {
-            color: 'hsl(270, 75%, 60%)',
-            label: 'Purple',
-          },
-        ],
-        columns: 5,
-        documentColors: 10,
-      },
+      // Configuration des couleurs - DISABLED
+      // fontColor: {
+      //   colors: [],
+      //   columns: 5,
+      //   documentColors: 10,
+      // },
+      //
+      // fontBackgroundColor: {
+      //   colors: [],
+      //   columns: 5,
+      //   documentColors: 10,
+      // },
 
       placeholder: placeholder,
     })
