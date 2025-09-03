289cbe0811e1123695778e84d630aed4a7e80d52
ZHAO <zhaoclaire11@gmail.com>
2025-09-03 11:37:16 +0200

    Enable the color feature in the CKEditor configuration.

--- assets/scripts/components/ckeditor.js
+++ assets/scripts/components/ckeditor.js
@@ -17,8 +17,6 @@ import {
   RemoveFormat, // styles de base > nettoyer
   FontSize, // styles > taille de police
   FontFamily, // styles > famille de police
-  FontColor, // couleurs > couleur de police
-  FontBackgroundColor, // couleurs > couleur de fond
   SpecialCharacters, // insertion > caractères spéciaux
   HorizontalLine, // insertion > ligne horizontale
   Image, // insertion > image
@@ -80,10 +78,6 @@ export function initializeCKEditor(elementId, placeholder = '') {
       FontSize,
       FontFamily,
 
-      // Couleurs (groupe colors)
-      FontColor,
-      FontBackgroundColor,
-
       // Édition (groupe editing)
       FindAndReplace,
     ],
@@ -101,11 +95,6 @@ export function initializeCKEditor(elementId, placeholder = '') {
         'bold',
         'italic',
         'removeFormat',
-        '|',
-
-        // Couleurs
-        'fontColor',
-        'fontBackgroundColor',
         '|',
 
         // Paragraphe
@@ -169,143 +158,6 @@ export function initializeCKEditor(elementId, placeholder = '') {
       insert: {
         integrations: ['url'],
       },
-    },
-
-    // Configuration des couleurs
-    fontColor: {
-      colors: [
-        {
-          color: 'hsl(0, 0%, 0%)',
-          label: 'Black',
-        },
-        {
-          color: 'hsl(0, 0%, 30%)',
-          label: 'Dim grey',
-        },
-        {
-          color: 'hsl(0, 0%, 60%)',
-          label: 'Grey',
-        },
-        {
-          color: 'hsl(0, 0%, 90%)',
-          label: 'Light grey',
-        },
-        {
-          color: 'hsl(0, 0%, 100%)',
-          label: 'White',
-          hasBorder: true,
-        },
-        {
-          color: 'hsl(0, 75%, 60%)',
-          label: 'Red',
-        },
-        {
-          color: 'hsl(30, 75%, 60%)',
-          label: 'Orange',
-        },
-        {
-          color: 'hsl(60, 75%, 60%)',
-          label: 'Yellow',
-        },
-        {
-          color: 'hsl(90, 75%, 60%)',
-          label: 'Light green',
-        },
-        {
-          color: 'hsl(120, 75%, 60%)',
-          label: 'Green',
-        },
-        {
-          color: 'hsl(150, 75%, 60%)',
-          label: 'Aquamarine',
-        },
-        {
-          color: 'hsl(180, 75%, 60%)',
-          label: 'Turquoise',
-        },
-        {
-          color: 'hsl(210, 75%, 60%)',
-          label: 'Light blue',
-        },
-        {
-          color: 'hsl(240, 75%, 60%)',
-          label: 'Blue',
-        },
-        {
-          color: 'hsl(270, 75%, 60%)',
-          label: 'Purple',
-        },
-      ],
-      columns: 5,
-      documentColors: 10,
-    },
-
-    fontBackgroundColor: {
-      colors: [
-        {
-          color: 'hsl(0, 0%, 0%)',
-          label: 'Black',
-        },
-        {
-          color: 'hsl(0, 0%, 30%)',
-          label: 'Dim grey',
-        },
-        {
-          color: 'hsl(0, 0%, 60%)',
-          label: 'Grey',
-        },
-        {
-          color: 'hsl(0, 0%, 90%)',
-          label: 'Light grey',
-        },
-        {
-          color: 'hsl(0, 0%, 100%)',
-          label: 'White',
-          hasBorder: true,
-        },
-        {
-          color: 'hsl(0, 75%, 60%)',
-          label: 'Red',
-        },
-        {
-          color: 'hsl(30, 75%, 60%)',
-          label: 'Orange',
-        },
-        {
-          color: 'hsl(60, 75%, 60%)',
-          label: 'Yellow',
-        },
-        {
-          color: 'hsl(90, 75%, 60%)',
-          label: 'Light green',
-        },
-        {
-          color: 'hsl(120, 75%, 60%)',
-          label: 'Green',
-        },
-        {
-          color: 'hsl(150, 75%, 60%)',
-          label: 'Aquamarine',
-        },
-        {
-          color: 'hsl(180, 75%, 60%)',
-          label: 'Turquoise',
-        },
-        {
-          color: 'hsl(210, 75%, 60%)',
-          label: 'Light blue',
-        },
-        {
-          color: 'hsl(240, 75%, 60%)',
-          label: 'Blue',
-        },
-        {
-          color: 'hsl(270, 75%, 60%)',
-          label: 'Purple',
-        },
-      ],
-      columns: 5,
-      documentColors: 10,
     },
 
     placeholder: placeholder,
